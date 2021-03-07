c7aa9ceb81700978d586c6d823a472e0717dd252
dunz <zergcake@gmail.com>
2021-03-07 12:01:14 +0900

    first commit

---
+++ test.js
@@ -0,0 +1,5 @@
+"use strict";
+var isPerson = function (obj) {
+    return obj.name !== undefined;
+};
+var str = 'asdf2';
---
+++ test.ts
@@ -0,0 +1,22 @@
+interface Person {
+    name: string;
+    age: number;
+}
+interface Car {
+    brand: string;
+    wheel: number;
+}
+
+const isPerson = (obj: any): obj is Person => {
+    return obj.name !== undefined;
+}
+//
+// const hello = (obj: Person | Car) => {
+//     if(isPerson(obj)) {
+//         obj.
+//     } else {
+//     }
+// }
+//
+
+const str = 'asdf2';
